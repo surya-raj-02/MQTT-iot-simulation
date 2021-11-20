370641004167b5ea05ee3d3978d9aeef4ddc8c4e
Swaroop <swaroopbhat710@gmail.com>
2021-11-21 01:32:58 +0530

    tinder

--- server.py
+++ server.py
@@ -1,42 +1,34 @@
 import paho.mqtt.client as mqtt
 import time
 import os
-from flask import Flask, request, render_template
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-app = Flask(__name__, static_folder='templates')
-app.config['DEBUG'] = True
 
 
 
 def on_message(client, userdata, message):
     print("received message: " ,str(message.payload.decode("utf-8")))
-    @app.route('/print_it', methods=['POST', 'GET'])
-    def print_it():
-        print("received message: " ,str(message.payload.decode("utf-8")))
-        return render_template('index.html', message=str(message.payload.decode("utf-8")))
-    return print_it()
 
 
 def on_message2(client, userdata, message):
     print("received message: " ,str(message.payload.decode("utf-8")))
-    @app.route('/print_it2', methods=['POST', 'GET'])
-    def print_it():
-        print("received message: " ,str(message.payload.decode("utf-8")))
-        return render_template('index.html', message=str(message.payload.decode("utf-8")))
-    return print_it()
+
+def on_message3(client, userdata, message):
+    print("received message: " ,str(message.payload.decode("utf-8")))
+
 
 mqttBroker ="test.mosquitto.org"
 
 client = mqtt.Client("Server1")
-client.connect(mqttBroker) 
-
 print("Connected to MQTT server!!!")
 
+
+client.connect(mqttBroker)
 client.loop_start()
+client.subscribe("iot-assignemnt-sss/#")
 
 client.message_callback_add("iot-assignemnt-sss/ultra-sensor", on_message)
 client.message_callback_add("iot-assignemnt-sss/force-sensor", on_message2)
-client.connect(mqttBroker)
-client.subscribe("iot-assignemnt-sss/#")
-time.sleep(30)
+client.message_callback_add("iot-assignemnt-sss", on_message3)
+
+
+time.sleep(300)
 client.loop_stop()
