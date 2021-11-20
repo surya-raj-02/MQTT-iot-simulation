bc933ec445495bf22cd921b321359d798dc13e82
surya-raj-02 <suryasandeepraj2002@gmail.com>
2021-11-21 00:57:28 +0530

    working rasp pi

--- r_pi.py
+++ r_pi.py
@@ -6,25 +6,25 @@ import csv
 
 mqttBroker = "test.mosquitto.org"
 client = mqtt.Client("rasp_pi")
-# client.connect(mqttBroker)
+client.connect(mqttBroker)
 
 print("Connected to MQTT server!!!")
 
 topic_list = [
-    "iot-assignemnt-sss-ultra-sensor", "iot-assignemnt-sss-force-sensor",
-    "iot-assignemnt-sss-gas-sensor", "iot-assignemnt-sss-motion-sensor"
+    "iot-assignemnt-sss/ultra-sensor", "iot-assignemnt-sss/force-sensor",
+    "iot-assignemnt-sss/gas-sensor", "iot-assignemnt-sss/motion-sensor"
 ]
 topic = "iot-assignment-sss"
 
-flag = int(input("Select '1' for sending data manually and '2' for sending through file"))
+flag = int(input("Select '1' for sending data manually and '2' for sending through file:"))
 if flag == 1:
     while True:
-        data_to_be_sent = input("Enter one hot encoded values in order: ultrasonic, force, gas, motion")
+        data_to_be_sent = input("Enter one hot encoded values in order: ultrasonic, force, gas, motion:")
         client.publish(topic,data_to_be_sent)
         print("Just published " + str(data_to_be_sent) + " to topic " + topic)
         time.sleep(1)
 elif flag == 2:
-    loc = input("Enter file location")
+    loc = input("Enter file location:")
     if loc == "":
         loc = "example.csv"
     with open(loc) as f:
@@ -35,4 +35,5 @@ elif flag == 2:
         client.publish(topic[1],i[1])
         client.publish(topic[2],i[2])
         client.publish(topic[3],i[3])
+        print("published:", i)
         time.sleep(1)
