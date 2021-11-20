bb8c9326894b7aa07af236f30d7164c86de53212
surya-raj-02 <suryasandeepraj2002@gmail.com>
2021-11-21 00:52:53 +0530

    Does something

--- r_pi.py
+++ r_pi.py
@@ -1,10 +1,12 @@
+from os import read
 import paho.mqtt.client as mqtt
 from random import randrange, uniform
 import time
+import csv
 
 mqttBroker = "test.mosquitto.org"
 client = mqtt.Client("rasp_pi")
-client.connect(mqttBroker)
+# client.connect(mqttBroker)
 
 print("Connected to MQTT server!!!")
 
@@ -12,10 +14,25 @@ topic_list = [
     "iot-assignemnt-sss-ultra-sensor", "iot-assignemnt-sss-force-sensor",
     "iot-assignemnt-sss-gas-sensor", "iot-assignemnt-sss-motion-sensor"
 ]
+topic = "iot-assignment-sss"
 
-while True:
-    input()
-    randNumber = uniform(20.0, 21.0)
-    client.publish("TEMPERATURE", randNumber)
-    print("Just published " + str(randNumber) + " to topic TEMPERATURE")
-    time.sleep(1)
+flag = int(input("Select '1' for sending data manually and '2' for sending through file"))
+if flag == 1:
+    while True:
+        data_to_be_sent = input("Enter one hot encoded values in order: ultrasonic, force, gas, motion")
+        client.publish(topic,data_to_be_sent)
+        print("Just published " + str(data_to_be_sent) + " to topic " + topic)
+        time.sleep(1)
+elif flag == 2:
+    loc = input("Enter file location")
+    if loc == "":
+        loc = "example.csv"
+    with open(loc) as f:
+        reader = csv.reader(f)
+        data = list(reader)
+    for i in data:
+        client.publish(topic[0],i[0])
+        client.publish(topic[1],i[1])
+        client.publish(topic[2],i[2])
+        client.publish(topic[3],i[3])
+        time.sleep(1)
